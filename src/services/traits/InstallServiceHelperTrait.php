1a0a95d0133c8b3c0952373d726740cbcbbc56cb
e.bachman <e.bachman@24hoursmedia.com>
2020-10-14 13:30:16 +0200

    Fix broken setup procedure in craft 3.5

--- src/services/traits/InstallServiceHelperTrait.php
+++ src/services/traits/InstallServiceHelperTrait.php
@@ -59,17 +59,20 @@ trait InstallServiceHelperTrait
      * Makes sure a field with a handle exists, if not retrieves the field from the callback and create it
      *
      * @param $handle
-     * @param callable $callback
+     * @param callable $createCallback                         callback that should return a new field if it does not exist
+     * @param callable $createdCallback                        callback after field is created, wi
      * @return bool|\craft\base\FieldInterface|null
      * @throws \Throwable
      */
-    protected function enforceFieldTypeWithHandle($handle, $callback) {
+    protected function enforceFieldTypeWithHandle($handle, $createCallback, $createdCallback = null) {
         $field = Craft::$app->fields->getFieldByHandle($handle);
         if (!$field) {
-            $field = $callback();
+            $field = $createCallback();
             if (!Craft::$app->fields->saveField($field, true)) {
+                /* @var $field \craft\base\Field */
                 return false;
             }
+            $createdCallback && $createdCallback($field);
         }
         return $field;
     }
